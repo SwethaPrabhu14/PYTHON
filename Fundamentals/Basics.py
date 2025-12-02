69eb15537345037be945aa701261a8121438e63a
SwethaPrabhu14 <prabhuswetha331@gmail.com>
2025-12-02 11:47:37 +0530

    Creating a New PYTHON Repo

---
+++ Fundamentals/Basics.py
@@ -0,0 +1,10 @@
+# ✅ Python Fundamentals (Beginner Level)
+# 1. Introduction to Python & Installation
+# 2. Variables and Data Types
+# 3. Input/Output
+# 4. Operators (Arithmetic, Logical, Comparison, Bitwise)
+# 5. Conditional statements (if, elif, else)
+# 6. Loops (for, while)
+# 7. break, continue, pass
+# 8. Type casting
+# 9. Strings: methods, slicing, formatting (f-strings)
