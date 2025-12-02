8fff097fd48f47c354995d31021ffd3390f942b4
SwethaPrabhu14 <prabhuswetha331@gmail.com>
2025-12-02 15:22:04 +0530

    Basics

--- Fundamentals/Basics.py
+++ Fundamentals/Basics.py
@@ -1,10 +1,51 @@
-# ✅ Python Fundamentals (Beginner Level)
-# 1. Introduction to Python & Installation
-# 2. Variables and Data Types
-# 3. Input/Output
-# 4. Operators (Arithmetic, Logical, Comparison, Bitwise)
-# 5. Conditional statements (if, elif, else)
-# 6. Loops (for, while)
-# 7. break, continue, pass
-# 8. Type casting
-# 9. Strings: methods, slicing, formatting (f-strings)
+print("Hello! PYTHON, Guido Van Rossum -> (1991)") # Basic Code in Python
+
+import sys
+print(sys.version) # Version
+
+a = 20 # Assigning a Values to Variables [a = int(10) -> Casting]
+b = float(20) # Type Conversion [B, b -> Case Sensitive]
+str = ' Swetha Lokeshwaran ' # Assigning a String to a Variable -> ' ' quote
+
+print(type(a)) # Check the Data Type
+
+# Global Keyword
+def myfunc():
+    global str
+    str = "Swetha Prabakaran" # -> " " quote
+myfunc()
+print("Output for Global Function:"+ str)
+
+# Built - in Modules
+import random
+print(random.randrange(1,10))
+
+# Strings:
+print(str[0]) # Strings are Array
+
+for  x in str: # Looping through a string
+    print(x)
+
+print(len(str)) # Length of a string
+
+print("Swetha" in str) # True
+print("Swetha" not in str) # False
+
+# String Slicing
+print(str[0:6]) # Start:End
+print(str[:6])
+print(str[2:])
+print(str[-5:-2])
+
+# Modifying Strings
+print(str.upper()) # Uppercase
+print(str.lower()) # Lowercase
+print(str.strip()) # Remove Whitespace
+print(str.replace("Swetha","Swey")) # Replacing
+print(str.split(",")) # Splitting
+
+# Concatenate
+print(str+"Lokii")
+
+# Format Strings
+print(f"My Name is {str} and age is {a}") # {str} -> Placeholder
